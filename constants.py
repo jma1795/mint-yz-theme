d9ebeea73a45617c6eb636d64ecb8c9e5ee98f05
SebastJava <sebastjava@hotmail.ca>
2022-02-09 18:02:01 -0500

    I can now delete y_hex_colors3 and y_hex_colors4 from the constants.py, generate-themes.py and update- variations.py. These were only used for the hover and pressed states on the old window close buttons, and those are not used anymore. I searched the whole directory, and now it can only be found in src/Mint-Y/ gtk-3.0/assets.svg for these old close buttons. Deleting these will make less boring and repetitive work when modifying the constants.py. This is true here on Mint-Yz, maybe not on the upstream Mint-Y.
    	modified:   Mint-Y-Colors/Mint-Y-Variations-3.svg
    	modified:   constants.py
    	modified:   generate-themes.py
    	modified:   src/Mint-Y/gtk-3.0/assets.svg
    	modified:   update-variations.py

--- constants.py
+++ constants.py
@@ -1,92 +1,45 @@
 #!/usr/bin/python3
 
-# Mint-X
-X_HEX_ACCENTS = ["#9ab87c", "#accd8a"]
-
-x_hex_colors = {}
-x_hex_colors["Aqua"] = "#6cabcd"
-x_hex_colors["Blue"] = "#5b73c4"
-x_hex_colors["Brown"] = "#aa876a"
-x_hex_colors["Grey"] = "#9d9d9d"
-x_hex_colors["Orange"] = "#db9d61"
-x_hex_colors["Pink"] = "#c76199"
-x_hex_colors["Purple"] = "#8c6ec9"
-x_hex_colors["Red"] = "#c15b58"
-x_hex_colors["Sand"] = "#c8ac69"
-x_hex_colors["Teal"] = "#5aaa9a"
-
-X_RGB_ACCENTS = ["172, 205, 138", "#accd8a"]
-
-x_rgb_colors = {}
-x_rgb_colors["Aqua"] = "108, 171, 205"
-x_rgb_colors["Blue"] = "91, 115, 196"
-x_rgb_colors["Brown"] = "170, 135, 106"
-x_rgb_colors["Grey"] = "157,157,157"
-x_rgb_colors["Orange"] = "219, 157, 97"
-x_rgb_colors["Pink"] = "199, 97, 153"
-x_rgb_colors["Purple"] = "140, 110, 201"
-x_rgb_colors["Red"] = "193, 91, 88"
-x_rgb_colors["Sand"] = "200, 172, 105"
-x_rgb_colors["Teal"] = "90, 170, 154"
-
-
 # Mint-Y
-Y_HEX_ACCENT1 = ["#92b372"]  # BASE
-Y_HEX_ACCENT2 = ["#8fa876"]  # DARK
-Y_HEX_ACCENT3 = ["#afca95"]  # PRELIGHT/HOVER
-Y_HEX_ACCENT4 = ["#779559"]  # PRESSED
+
+# I can now delete y_hex_colors3 and y_hex_colors4 from the constants.py, generate-themes.py and update-
+# variations.py. These were only used for the hover and pressed states on the old window close buttons, and
+# those are not used anymore. I searched the whole directory, and now it can only be found in src/Mint-Y/
+# gtk-3.0/assets.svg for these old close buttons. Deleting these will make less boring and repetitive work
+# when modifying the constants.py. This is true here on Mint-Yz, maybe not on the upstream Mint-Y.
+
+Y_HEX_ACCENT1 = ["#92b372"]  # BASE (You can't change these; sed uses them for search and replace.)
+Y_HEX_ACCENT2 = ["#8fa876"]  # DARK (You can't change these; sed uses them for search and replace.)
 
 y_hex_colors1 = {}
 y_hex_colors2 = {}
-y_hex_colors3 = {}
-y_hex_colors4 = {}
 
 y_hex_colors1["BlueBelize"] = "#2980B9"
 y_hex_colors2["BlueBelize"] = "#277AB0"
-y_hex_colors3["BlueBelize"] = "#5499C7"
-y_hex_colors4["BlueBelize"] = "#1D5A82"
 
 y_hex_colors1["Grey"] = "#767676"
 y_hex_colors2["Grey"] = "#767676"
-y_hex_colors3["Grey"] = "#919191"
-y_hex_colors4["Grey"] = "#535353"
 
 y_hex_colors1["MintSoda"] = "#29BF68"
 y_hex_colors2["MintSoda"] = "#27B663"
-y_hex_colors3["MintSoda"] = "#54CC86"
-y_hex_colors4["MintSoda"] = "#1D8649"
 
 y_hex_colors1["MintGum"] = "#1BBB9B"
 y_hex_colors2["MintGum"] = "#1AB294"
-y_hex_colors3["MintGum"] = "#49C9AF"
-y_hex_colors4["MintGum"] = "#13836D"
 
 y_hex_colors1["BlueElectron"] = "#0984E3"
 y_hex_colors2["BlueElectron"] = "#097ED8"
-y_hex_colors3["BlueElectron"] = "#3A9DE9"
-y_hex_colors4["BlueElectron"] = "#065D9F"
 
 y_hex_colors1["Purple"] = "#8F4CB1"
 y_hex_colors2["Purple"] = "#8848A9"
-y_hex_colors3["Purple"] = "#A570C1"
-y_hex_colors4["Purple"] = "#64357C"
 
 y_hex_colors1["RedShine"] = "#FF1A2C"
 y_hex_colors2["RedShine"] = "#F20D1F"
-y_hex_colors3["RedShine"] = "#FF4856"
-y_hex_colors4["RedShine"] = "#B3121F"
 
 y_hex_colors1["OrangeShine"] = "#FF791A"
 y_hex_colors2["OrangeShine"] = "#FF791A"
-y_hex_colors3["OrangeShine"] = "#FF9448"
-y_hex_colors4["OrangeShine"] = "#B35512"
 
 y_hex_colors1["AquaShine"] = "#00ABE5"
 y_hex_colors2["AquaShine"] = "#00A3DA"
-y_hex_colors3["AquaShine"] = "#33BCEA"
-y_hex_colors4["AquaShine"] = "#0078A1"
 
 y_hex_colors1["Pink"] = "#FF4CA5"
 y_hex_colors2["Pink"] = "#F3489D"
-y_hex_colors3["Pink"] = "#FF70B7"
-y_hex_colors4["Pink"] = "#B33574"
