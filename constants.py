3598db045c6a0887007223f05f7147e1239a3929
SebastJava <sebastjava@hotmail.ca>
2022-03-01 16:00:01 -0500

    modified:   constants.py (AquaShine 195,90,43)
    	modified:   Mint-Y-Colors/Mint-Y-Variations-compare-OLD.png
    	modified:   Mint-Y-Colors/Mint-Y-Variations.png
    	modified:   Mint-Y-Colors/Mint-Y-Variations.svg

--- constants.py
+++ constants.py
@@ -35,8 +35,8 @@ y_hex_colors2["RedShine"] = "#E50012"
 y_hex_colors1["OrangeShine"] = "#FF791A"
 y_hex_colors2["OrangeShine"] = "#FF791A"
 
-y_hex_colors1["AquaShine"] = "#009FD4"
-y_hex_colors2["AquaShine"] = "#009CCE"
+y_hex_colors1["AquaShine"] = "#0BA0D1"
+y_hex_colors2["AquaShine"] = "#0BA0D1"
 
 y_hex_colors1["Pink"] = "#F656A5"
 y_hex_colors2["Pink"] = "#EB4798"
